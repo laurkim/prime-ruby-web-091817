cb674b00d8b6dcb99c3e7081230cf92a9c9d1205
laurkim <laurakeem@gmail.com>
2017-09-02 23:08:42 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,15 @@
-# Add  code here!
+# Add code here!
+
+def prime?(number)
+  if number < 2
+    return false
+  end
+
+  range = (2...number).to_a
+  range.each do |num|
+    if number % num == 0
+      return false
+    end
+  end
+  true
+end
